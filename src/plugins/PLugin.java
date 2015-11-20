4b2175438b12408b3a02f1edbbbb5a46fb53c641
fatimaSoufiane <fatimaezzahrac@gmail.com>
2015-11-20 11:09:34 +0100

    fisrt step

---
+++ src/plugins/PLugin.java
@@ -0,0 +1,9 @@
+package plugins;
+
+public interface PLugin {
+	public String transforms(String s);
+	public String getLebel();
+	
+	
+
+}
---
+++ src/plugins/PluginFilter.java
@@ -0,0 +1,13 @@
+package plugins;
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+public abstract class PluginFilter implements FilenameFilter{
+
+	public boolean accept(File dir, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
